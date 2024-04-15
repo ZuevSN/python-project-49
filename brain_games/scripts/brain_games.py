85af4f626a6be590eecb5ed367bada8e900b174b
Sergei Zuev <zuevsn86@gmail.com>
2024-04-16 00:17:03 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import prompt
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
 
 def main():
     print("Welcome to the Brain Games!")
-
+    welcome_user()
 
 if __name__ == "__main__":
     main()

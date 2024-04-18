68962fd8389de2609121737aa961621551b3bf4e
ZuevSN <zuevsn86@gmail.com>
2024-04-18 13:24:30 +0300

    step 6

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.scripts import cli
-from brain_games.scripts import brain_even
 
 
 def main():
     print("Welcome to the Brain Games!")
     name = cli.welcome_user()
-    brain_even.main(name)
 
 
 if __name__ == "__main__":

1006bdeb256d6e41c0e6c5a15238c84eda44d3c0
ZuevSN <zuevsn86@gmail.com>
2024-04-18 16:16:24 +0300

    step 7

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,27 @@
+import prompt
+import random
+import math
+from brain_games.scripts import cli
+from brain_games.scripts import brain_even
+
+
+def nod():
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+    print(f'Question: {x} {y}')
+    result = math.gcd(x, y)
+    return result
+
+def main():
+    name = cli.welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    score = 0
+    end = False
+    while not end:
+        result = nod()
+        answer = prompt.string('Your answer: ')
+        score, end = brain_even.respondent(name, answer, result, score)
+
+
+if __name__ == "__main__":
+    main()

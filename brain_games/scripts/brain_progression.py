b0929cfa127ec5a4a4f947a89912f62b4502be69
ZuevSN <zuevsn86@gmail.com>
2024-04-19 11:54:57 +0300

    step 8

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -5,21 +5,19 @@ from brain_games.scripts import brain_even
 
 
 def progression():
-    #   limits = [(1, 100),(1, 100),(5, 10)]
-    #       for limits in question[2]:
-    #           args.append(random.randint(limits[0], limits[1]))
     a1 = random.randint(1, 100)
     step = random.randint(1, 100)
     amount = random.randint(5, 10)
     progression_string = ''
-    random_num = random.randint(0, amount - 1)
+    random_position = random.randint(0, amount - 1)
+    # формирую отображаемую строку
     for i in range(0, amount):
-        if i == random_num:
+        if i == random_position:
             result = a1 + i * step
             progression_string = progression_string + ' ' + '..'
         else:
             progression_string = progression_string + ' ' + str(a1 + i * step)
-    print(f'Question: {progression_string}')
+    print(f'Question:{progression_string}')
     return result
 
 

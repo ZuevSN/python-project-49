3dd3ad82e02f1de780d2424b76d533d881f251e3
Sergei Zuev <zuevsn@gmail.com>
2024-04-21 02:03:16 +0300

    fix

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,6 +6,7 @@ from brain_games.scripts import brain_even
 def sum_arithm_progr(a1: int, step: int, amount: int):
     return int((amount * (2 * a1 + step * (amount - 1))) / 2)
 
+
 # автотесты не рассчитаны на calc
 def calc():
     # В списке кортежы из функции, отображения вопроса,
@@ -15,9 +16,9 @@ def calc():
         (lambda x, y: x + y, lambda x, y: f"{x} + {y}", [(10, 100), (10, 100)]),
         (lambda x, y: x - y, lambda x, y: f"{x} - {y}", [(10, 100), (10, 100)]),
         (lambda x, y: x * y, lambda x, y: f"{x} * {y}", [(1, 10), (1, 10)]),
+        (lambda x: x ** 2, lambda x: f"{x}^2", [(2, 10)]),
         (lambda x, y: x // y, lambda x, y: f"{x} // {y}", [(1, 100), (1, 100)]),
         (lambda x, y: x % y, lambda x, y: f"{x} % {y}", [(1, 100), (1, 100)]),
-        (lambda x: x ** 2, lambda x: f"{x}^2", [(2, 10)]),
         (lambda x: math.factorial(x), lambda x: f"{x}!", [(2, 5)]),
         (sum_arithm_progr, lambda a1, step, amount: f"Арифметическая \
 прогрессия. Первый член прогрессии - {a1}, \
@@ -50,6 +51,7 @@ def simple_calc():
     print(f"Question: {question[1](*args)}")
     result = question[0](*args)
     return result
+
 
 def main():
     print("Welcome to the Brain Games!")
